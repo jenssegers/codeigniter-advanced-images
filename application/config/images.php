4f26f6edc2ebbaca4ced9dfd62753b0c02ac914e
Jens Segers <segers.jens@gmail.com>
2012-01-06 15:49:22 +0100

    Better examples

--- application/config/images.php
+++ application/config/images.php
@@ -16,11 +16,11 @@
 |
 */
 
-$config["image_sizes"]["square"] = array(400, 400);
-$config["image_sizes"]["rectangle"] = array(600, 400);
-$config["image_sizes"]["long"] = array(200, 600);
+$config["image_sizes"]["square"] = array(100, 100);
+$config["image_sizes"]["long"] = array(280, 600);
 $config["image_sizes"]["wide"] = array(600, 200);
+$config["image_sizes"]["hero"] = array(940, 320);
 
-$config["image_sizes"]["small"] = array(300, 0);
-$config["image_sizes"]["medium"] = array(500, 0);
+$config["image_sizes"]["small"] = array(280, 0);
+$config["image_sizes"]["medium"] = array(340, 0);
 $config["image_sizes"]["large"] = array(800, 0);
