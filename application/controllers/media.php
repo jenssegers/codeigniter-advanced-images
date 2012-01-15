5ea32daa8e2bd53fdfee02f774c6e4caa4597c87
Jens Segers <segers.jens@gmail.com>
2012-01-15 13:49:52 +0100

    Possible to add preset to image instead of dimensions

--- application/controllers/media.php
+++ application/controllers/media.php
@@ -34,43 +34,50 @@ class Media extends CI_Controller {
         $path = $this->uri->uri_string();
         $pathinfo = pathinfo($path);
         $size = end(explode("-", $pathinfo["filename"]));
-        $original = $pathinfo["dirname"] . "/" . str_replace("-" . $size, "", $pathinfo["basename"]);
+        $original = $pathinfo["dirname"] . "/" . str_ireplace("-" . $size, "", $pathinfo["basename"]);
         
         // original image not found, show 404
         if (!file_exists($original)) {
             show_404($original);
         }
         
-        @list($width, $height) = explode("x", $size);
+        // load the allowed image sizes
+        $this->load->config("images");
+        $sizes = $this->config->item("image_sizes");
+        $allowed = FALSE;
         
-        // only continue with a valid width and height
-        if ($width >= 0 && $height >= 0) {
-            
-            // load the allowed image sizes
-            $this->load->config("images");
-            $sizes = $this->config->item("image_sizes");
+        if (stripos($size, "x") !== FALSE) {
+            // dimensions are provided as size
+            @list($width, $height) = explode("x", $size);
             
             // security check, to avoid users requesting random sizes
-            $allowed = FALSE;
             foreach ($sizes as $s) {
                 if ($width == $s[0] && $height == $s[1]) {
                     $allowed = TRUE;
-                    break; // nasty
+                    break;
                 }
             }
+        } else if (isset($sizes[$size])) {
+            // optional, the preset is provided instead of the dimensions
+            // NOTE: the controller will be executed EVERY time you request the image this way
+            @list($width, $height) = $sizes[$size];
+            $allowed = TRUE;
             
-            if ($allowed) {
-                // initialize library
-                $config["source_image"] = $original;
-                $config['new_image'] = $path;
-                $config["width"] = $width;
-                $config["height"] = $height;
-                $config["dynamic_output"] = FALSE; // always save as cache
-                
-
-                $this->load->library('image_lib', $config);
-                $this->image_lib->fit();
-            }
+            // set the correct output path
+            $path = str_ireplace($size, $width . "x" . $height, $path);
+        }
+        
+        // only continue with a valid width and height
+        if ($allowed && $width >= 0 && $height >= 0) {
+            // initialize library
+            $config["source_image"] = $original;
+            $config['new_image'] = $path;
+            $config["width"] = $width;
+            $config["height"] = $height;
+            $config["dynamic_output"] = FALSE; // always save as cache
+            
+            $this->load->library('image_lib', $config);
+            $this->image_lib->fit();
         }
         
         // check if the resulting image exists, else show the original
